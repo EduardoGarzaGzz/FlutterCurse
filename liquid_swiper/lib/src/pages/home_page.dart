e5cdfb09c557f713971d6661a74d66b6376753f6
Eduardo <eduardo.garza.gzz@outlook.es>
2020-01-29 08:51:48 -0600

    Liquid swiper

---
+++ liquid_swiper/lib/src/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/Constants/Helpers.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:liquid_swiper/src/widgets/liquid_pages.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: liquidPages,
+        fullTransitionValue: 300,
+        enableLoop: true,
+        enableSlideIcon: true,
+        positionSlideIcon: 0.8,
+        waveType: WaveType.liquidReveal,
+        onPageChangeCallback: (page)=>  this._pageChangeCallback(page),
+        currentUpdateTypeCallback: (update) => this._updateTypeCallback(update),
+      )
+    );
+  }
+
+  _pageChangeCallback(int page) {}
+
+  _updateTypeCallback(UpdateType update) {}
+}
---
+++ liquid_swiper/lib/src/widgets/liquid_pages.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+final styleText = TextStyle(fontSize: 20.0, color: Colors.white);
+
+final liquidPages = [
+  Container(
+    color: Colors.pinkAccent,
+    child: Center(
+      child: Text(
+        'Slide 1',
+        style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.deepPurpleAccent,
+    child: Center(
+      child: Text(
+        'Slide 2',
+        style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.greenAccent,
+    child: Center(
+      child: Text(
+        'Slide 3',
+        style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.blueAccent,
+    child: Center(
+      child: Text(
+        'Slide 4',
+        style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.orangeAccent,
+    child: Center(
+      child: Text(
+        'Slide 5',
+        style: styleText,
+      ),
+    ),
+  ),
+];
