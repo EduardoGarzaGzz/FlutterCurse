3b57ad44f0e775a13584308af4694008306c0f90
Eduardo Uriel Garza Gonzalez <EDUARDO.URIEL.GARZA.GONZALEZ@AMWAY.COM>
2022-02-02 18:51:15 -0600

    New app fl_components part 1

---
+++ fl_components/lib/screens/listview2_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  final options = const ['Mega man', 'Metal Gear', 'Super Smash', 'Final Fantasy'];
+
+  const ListView2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView1'),
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(options[index]),
+                  trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: options.length));
+  }
+}
